3a56fe1ef70362d59976af42da0e322f46b445b8
csmyth12 <conorsmyth12@gmail.com>
2024-03-21 13:36:50 +0000

    mobile style fixes

--- src/components/FinalList.tsx
+++ src/components/FinalList.tsx
@@ -19,7 +19,7 @@ export const FinalList: FunctionComponent<FinalListProps> = ({ list, listName, d
     copyString += `${'\n'}csmyth12.github.io/list-ranker/${router.pathname}`
 
     return (
-        <div>
+        <div className='p-4'>
             <h2 className='text-4xl'>{`Here's your ${listName} Ranking!`}</h2>
 
             <ol className='text-xl odd:text-gray-400 even:text-black p-4'>
@@ -29,8 +29,19 @@ export const FinalList: FunctionComponent<FinalListProps> = ({ list, listName, d
                     </li>
                 ))}
             </ol>
-            <div className='flex flex-row items-end justify-end w-full'>
-                <button className='rounded border-2 bg-rose-300 text-white py-4 px-8 hover:bg-rose-500' onClick={() => copy(copyString)}>Copy</button>
+            <div className='flex flex-row items-center justify-between w-full'>
+                <button 
+                    className='rounded border-2 bg-gray-300 py-4 px-8 hover:bg-gray-500'
+                    onClick={() => router.reload()}
+                >
+                    Start again
+                </button>
+                <button 
+                    className='rounded border-2 bg-rose-300 text-white py-4 px-8 hover:bg-rose-500'
+                    onClick={() => copy(copyString)}
+                >
+                    Copy
+                </button>
             </div>
             
             {!!disclaimer && (
--- src/components/OptionButton.tsx
+++ src/components/OptionButton.tsx
@@ -1,30 +1,33 @@
 import { RankerOption } from '@/types/RankerOption'
-import { FunctionComponent } from 'react'
+import { FunctionComponent, useEffect, useRef } from 'react'
 
 export interface OptionProps {
     option: RankerOption
     onSelect: (arg0: RankerOption) => void
 }
 
-export const OptionButton: FunctionComponent<OptionProps> = ({ option, onSelect }) => (
-    <button 
-        className={
-            'w-[80%] lg:w-1/2 h-[45%] lg:h-[80%] flex flex-col rounded-xl'
-            + ' justify-center items-center border-2 border-rose-400 mx-1 lg:mx-24 my-4'
-            + ' transition ease-in-out duration-100 hover:scale-105 hover:bg-rose-300'
-            + ' active:scale-105 active:bg-rose-500 focus:scale-105 focus:bg-rose-300'
-        }
+export const OptionButton: FunctionComponent<OptionProps> = ({ option, onSelect }) => {
 
-        onClick={(e) => {
-            onSelect(option)
-            e.currentTarget.blur()
-        }}
-    >
-        <h3 className='h-8 w-full text-2xl my-4'>{option.name}</h3>
-        <img 
-            src={option.image}
-            alt={`${option.name} picture`}
-            className='max-w-full max-h-[90%] min-w-[40%] min-h-[71%] m-2 lg:mx-16 rounded-lg'
-        />
-    </button>
+    return (
+        <button
+            className={
+                'w-[80%] lg:w-1/2 h-[45%] lg:h-[80%] flex flex-col rounded-xl'
+                + ' justify-center items-center border-2 border-rose-400 mx-1 lg:mx-24 my-4'
+                + ' transition ease-in-out duration-100 desktophover:hover:scale-105 desktophover:hover:bg-rose-300'
+                + ' focus:scale-105 focus:bg-rose-300 active:scale-105 active:bg-rose-500'
+            }
+
+            onClick={(e) => {
+                onSelect(option)
+                e.currentTarget.blur()
+            }}
+         >
+            <h3 className='h-8 w-full text-2xl my-4'>{option.name}</h3>
+            <img 
+                src={option.image}
+                alt={`${option.name} picture`}
+                className='max-w-full max-h-[90%] min-w-[40%] min-h-[71%] m-2 p-2 lg:mx-16 rounded-2xl'
+            />
+        </button>
 )
+}
